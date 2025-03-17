f64148bf673c59a5deac20c4588fdfa928a70dc5
jerichoji <3120220353@bit.edu.cn>
2025-03-18 00:28:54 +0800

    docker images

---
+++ README.md
@@ -0,0 +1,9 @@
+
+```bash
+git clone https://gitee.com/jerichoji/LIVO_SLAM.git
+cd LIVO_SLAM
+docker pull crpi-6bf8f4ebks7rgb7z.cn-beijing.personal.cr.aliyuncs.com/jerichoji/fast_livo2:latest
+# run
+cd LIVO_SLAM
+docker.sh
+```
--- docker.sh
+++ docker.sh
@@ -1,10 +1,10 @@
 docker run -it \
+  --rm \
   --gpus '"device=0,1"' \
   --shm-size=128g \
-  -v /home/liji/catkin_ws/src/:/catkin_ws/src/ \
+  -v ~/LIVO_SLAM/src/:/catkin_ws/src/ \
   -v /home/data/Dataset/fast_livo2:/catkin_ws/dataset/ \
   --env="DISPLAY=$DISPLAY"\
   --name fast_livo2 \
-  fast_livo2:v2
-#   ros:noetic-perception-focal
+  fast_livo2:latest
   
