8ce0fad5c89b46b67b15000815c9a3d8d6f3499b
jerichoji <3120220353@bit.edu.cn>
2025-03-20 14:49:48 +0800

    topic in real

--- docker.sh
+++ docker.sh
@@ -1,9 +1,9 @@
 docker run -it \
   --rm \
-  --gpus '"device=0,1"' \
-  --shm-size=128g \
+  --gpus all \
+  --shm-size=32g \
   -v ~/LIVO_SLAM/src/:/catkin_ws/src/ \
-  -v /home/data/Dataset/fast_livo2:/catkin_ws/dataset/ \
+  # -v /home/data/Dataset/fast_livo2:/catkin_ws/dataset/ \
   --env="DISPLAY=$DISPLAY"\
   --name fast_livo2 \
   fast_livo2:latest
